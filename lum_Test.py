55a0bb9e6c43d71c5aefd7f1e7016d90a7024234
alexeiddg <a01637405@tec.mx>
2023-11-09 12:00:56 -0600

    Luminosidad funciona???

--- lum_Test.py
+++ lum_Test.py
@@ -1,67 +1,52 @@
 from PIL import Image
 import numpy as np
+import os
 
 # Parámetros
 MIN_INTENSITY = 170
 MAX_INTENSITY = 250
-
-
-def get_quadrants(img, x_quad=2, y_quad=4):
-    # Dividir imagen en cuadrantes
-    y_sections = np.array_split(img, y_quad, axis=0)
-
-    quadrants = []
-    for section in y_sections:
-        x_sections = np.array_split(section, x_quad, axis=1)
-        quadrants.extend(x_sections)
-
-    return quadrants
-
+IMAGE_FOLDER = 'Images'  # Asegúrate de actualizar esto a la ubicación de tus imágenes
+IMAGE_EXTENSIONS = ['.png']  # Las extensiones de archivos que quieres procesar
 
 def sum_channels(channels):
     red = channels[0]
     green = channels[1]
     blue = channels[2]
+    return 0.2989 * red + 0.5870 * green + 0.1140 * blue
 
-    sum = 0.2989 * red + 0.5870 * green + 0.1140 * blue
-
-    return round(sum, 2)
-
-
-def check_illumination(img_path):
-    # Leer imagen
+def check_illumination_focus_left(img_path, focus_area=(0.25, 0.5, 0.25, 0.5)):
     img = Image.open(img_path).convert('RGB')
-
-    # Convertir a array
     img_array = np.array(img)
-
-    # Dividir en cuadrantes
-    quadrants = get_quadrants(img_array)
-
-    status = "GO"
-
-    for q in quadrants:
-
-        # Promediar canales
-        red = q[:, :, 0].mean()
-        green = q[:, :, 1].mean()
-        blue = q[:, :, 2].mean()
-
-        # Sumar canales
-        intensity = sum_channels([red, green, blue])
-
-        # Verificar límites
-        if not (MIN_INTENSITY <= intensity <= MAX_INTENSITY):
-            status = "NO GO"
-            break
-
+    w, h = img.size
+    left = int(w * focus_area[0])
+    upper = int(h * focus_area[1])
+    right = int(w * focus_area[2]) + left
+    lower = int(h * focus_area[3]) + upper
+    focus_array = img_array[upper:lower, left:right]
+    red = focus_array[:, :, 0].mean()
+    green = focus_array[:, :, 1].mean()
+    blue = focus_array[:, :, 2].mean()
+    intensity = sum_channels([red, green, blue])
     return {
-        "status": status
+        "status": "GO" if MIN_INTENSITY <= intensity <= MAX_INTENSITY else "NO GO",
+        "intensity": intensity,
+        "red": red,
+        "green": green,
+        "blue": blue
     }
 
+def process_images(image_folder):
+    for file_name in os.listdir(image_folder):
+        if any(file_name.lower().endswith(ext) for ext in IMAGE_EXTENSIONS):
+            img_path = os.path.join(image_folder, file_name)
+            result = check_illumination_focus_left(img_path)
+            print(f"Image: {file_name}")
+            print(f"Status: {result['status']}")
+            print(f"Intensity: {result['intensity']:.2f}")
+            print(f"Red: {result['red']:.2f}")
+            print(f"Green: {result['green']:.2f}")
+            print(f"Blue: {result['blue']:.2f}")
+            print("-" * 30)
 
-# Ejemplo
-test_img = 'Images/REF_23.PNG'
-result = check_illumination(test_img)
-
-print(result["status"])
+# Ejemplo de uso
+process_images(IMAGE_FOLDER)
